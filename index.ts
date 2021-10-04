bddcb2e468c9d4c5d021506ddc7fd59747738eda
pics78 <mcmc.k0206@gmail.com>
2021-10-04 22:07:16 +0900

    test connection to postgresql

--- index.ts
+++ index.ts
@@ -1,30 +1,36 @@
-import express, { Application } from 'express';
-import { Pool } from 'pg';
-
-// require('dotenv').config();
+import { Express, Request, Response } from 'express';
+import { Pool, PoolClient, QueryResult } from 'pg';
 
 const PORT = process.env.npm_package_config_port;
+
+const app: Express = require('express')();
 
 const pool: Pool = new Pool({
     host:       process.env.POSTGRESQL_HOST,
     database:   process.env.POSTGRESQL_DATABASE,
     user:       process.env.POSTGRESQL_USER,
     port:       process.env.POSTGRESQL_PORT,
-    password:   process.env.POSTGRESQL_PASS
+    password:   process.env.POSTGRESQL_PASS,
+    ssl:        { rejectUnauthorized: false }
 });
 
-pool.connect((err, poolClient) => {
-    if (err) {
-        console.log(err);
-    } else {
-        poolClient.query('select * from reminders', (err, result) => {
-            console.log(result.rows);
-        });
-    }
+const pgConnectTest = async (query: string) => {
+    pool.connect((err: Error, poolClient: PoolClient) => {
+        if (err) {
+            console.log(err);
+        } else {
+            poolClient.query(query, (err: Error, result: QueryResult<any>) => {
+                console.log(result.rows);
+            });
+        }
+    });
+};
+
+app.get('/', (req: Request, res: Response) => {
+    pgConnectTest('select * from reminders');
+    res.send(JSON.stringify({'status': 'OK'}));
 });
 
-const app: Application = express();
-
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}.`);
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Starting Heroku App.`);
 });
